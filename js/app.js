197004490ee0171bf858090f91138b9ebee3430d
andela-aonawale <ahmed.onawale@andela.co>
2015-03-10 18:56:22 +0100

    fixed the crazy delete bug

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 var app = angular.module('CommentsApp', []);
 app.controller('CommentsController', function(){
     this.comments = [];
-    this.comment = {text: '', name: '', email: '', date: new Date(), show: true, hide: false, upvote: 0, downvote: 0};
+    this.comment = {text: '', name: '', email: '', date: new Date(), sh: true, show: true, hide: false, upvote: 0, downvote: 0};
     this.addComment = function(){
         this.comments.push(this.comment);
-        this.comment = {date: new Date(), show: true, hide: false, upvote: 0, downvote: 0};
+        this.comment = {date: new Date(), sh: true, show: true, hide: false, upvote: 0, downvote: 0};
     };
-    this.deleteComment = function(){
-        this.comments.splice(this.comments.indexOf(this)-1, 1);
+    this.deleteComment = function(com){
+        //a cockroach came into the splice below, so i had to use an alternative
+        //this.comments.splice(this.comments.indexOf(this)-1, 1);
+        com.sh = false;
     };
     this.editComment = function(com){
         com.hide = true;
@@ -19,13 +21,8 @@ app.controller('CommentsController', function(){
     };
     this.addVote = function(com){
         ++com.upvote;
-    }
+    };
     this.remVote = function(com){
             ++com.downvote;
-    }
-}).run(function(){
-    //localStorage.setItem('comments', JSON.stringify(this.comments));
-    //$(document).ready(function(){
-    //console.log(JSON.parse(localStorage.getItem('comments')));
-    //});
+    };
 });
